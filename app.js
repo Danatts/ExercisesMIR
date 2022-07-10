c53a769682ccd4578c697fec8d8c907d70ded15f
Daniel <danielatts97@gmail.com>
2022-07-09 22:41:12 -0500

    fourth exercise

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  const nombre = req.query.nombre;
-  res.send(`<h1>Hola ${nombre ? nombre : 'desconocido'}!</h1>`);
+const capitalize = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+app.get('/makers/:name', (req, res) => {
+  let name = req.params.name;
+  const newName = capitalize(name);
+  res.send(`<h1>Hola ${name ? newName : 'Desconocido'}!</h1>`);
 });
 
 const port = 3000;
