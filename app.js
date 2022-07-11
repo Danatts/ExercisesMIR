9d2c1177c1f96db156844cfe4c6b1c7691f7ca0d
Daniel <danielatts97@gmail.com>
2022-07-10 20:03:33 -0500

    seventh exercise

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
-  res.render('index');
+  const browser = req.headers['user-agent'];
+  res.send(`<h1>${browser}</h1>`);
 });
-
-app.post('/', (req, res) => {
-  const name = req.body.name;
-  res.send(`<h1>Hola ${name}!</h1>`);
-});
-
 
 const port = 3000;
 app.listen(port, () => {
